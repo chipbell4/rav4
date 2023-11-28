b04da17530d6a71e106db898322f1a17ce45edf8
Chip Bell <chipbell4@gmail.com>
2023-11-27 22:25:53 -0500

    Formatter

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-import * as THREE from 'three';
-import { camera } from './camera';
-import { TriangleMesh } from './triangle-mesh';
+import * as THREE from "three";
+import { camera } from "./camera";
+import { TriangleMesh } from "./triangle-mesh";
 
 const scene = new THREE.Scene();
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 const triangle = new TriangleMesh();
 
@@ -15,8 +15,8 @@ scene.add(triangle);
 camera.position.z = 5;
 
 function animate() {
-	requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 
-	renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 animate();
--- triangle-mesh.js
+++ triangle-mesh.js
@@ -5,31 +5,37 @@ import { BufferGeometry, MeshBasicMaterial } from "three";
 import { BufferAttribute, Mesh, DoubleSide } from "three";
 
 export class TriangleMesh extends Mesh {
-    constructor() {
-        const vertices = new Float32Array([
-            0, 0, 0, // v0
-            1, 0, 0, // v1
-            1, 1, 0, // v2
-        ]);
+  constructor() {
+    const vertices = new Float32Array([
+      0,
+      0,
+      0, // v0
+      1,
+      0,
+      0, // v1
+      1,
+      1,
+      0, // v2
+    ]);
 
-        const geometry = new BufferGeometry();
-        geometry.setAttribute("position", new BufferAttribute(vertices, 3));
+    const geometry = new BufferGeometry();
+    geometry.setAttribute("position", new BufferAttribute(vertices, 3));
 
-        const material = new MeshBasicMaterial({
-            color: 0xff0000,
-            side: DoubleSide,
-        });
+    const material = new MeshBasicMaterial({
+      color: 0xff0000,
+      side: DoubleSide,
+    });
 
-        super(geometry, material);
+    super(geometry, material);
 
-        // Add the wireframe as well, so we can color the edges
-        // see: https://stackoverflow.com/a/40858869
-        const wireframeGeometry = new EdgesGeometry(this.geometry);
-        const wireframeMaterial = new LineBasicMaterial({
-            color: 0xffffff,
-            linewidth: 5,
-        });
-        const edges = new LineSegments(wireframeGeometry, wireframeMaterial);
-        this.add(edges);
-    }
+    // Add the wireframe as well, so we can color the edges
+    // see: https://stackoverflow.com/a/40858869
+    const wireframeGeometry = new EdgesGeometry(this.geometry);
+    const wireframeMaterial = new LineBasicMaterial({
+      color: 0xffffff,
+      linewidth: 5,
+    });
+    const edges = new LineSegments(wireframeGeometry, wireframeMaterial);
+    this.add(edges);
+  }
 }
